632df4fb5ff837affd4258150fa4f4d6938f5678
breagan <github@airetraffic.com>
2015-07-25 15:30:01 -0400

    Create stop.lua

---
+++ stop.lua
@@ -0,0 +1,13 @@
+--  Sets all gpios to LOW and resets variables.
+collectgarbage()
+tmr.stop(0)
+tmr.stop(1)
+tmr.stop(2)
+gpio.write(zone0, gpio.LOW)
+gpio.write(zone1, gpio.LOW)
+gpio.write(zone2, gpio.LOW)
+duration = nil
+multiplyer = nil
+zone0time = nil
+zone1time = nil
+zone2time = nil
