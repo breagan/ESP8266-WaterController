0e80e051c04f5a0d59436eead3d341a824a2921f
Bruce Reagan <github@airetraffic.com>
2015-08-01 11:47:21 -0400

    changed dofile from lua to lc

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 wifi.setmode(wifi.STATION);
 wifi.sta.config("SSID","password"); --  Enter your SSID and password here.
-cfg = {ip="192.168.11.44",netmask="255.255.255.0",gateway="192.168.11.1"};  -- IP number for your ESP and gateway.  Use a static number and reserve it in your router. 
+cfg = {ip="192.168.11.xx",netmask="255.255.255.0",gateway="192.168.11.1"};  -- IP number for your ESP and gateway.  Use a static number and reserve it in your router. 
 wifi.sta.setip(cfg);   -- sends ip to ESP
 zone0 = 1      --gpio4  markings are reversed on chip with gpio5
 zone1 = 5      --gpio14
@@ -13,4 +13,5 @@ gpio.mode(zone2, gpio.OUTPUT)
 gpio.write(zone2, gpio.LOW)
 
 collectgarbage()
-dofile("servernode.lua") --  starts the server script.  comment this out while depugging.  Start the server manually untill you are sure it is working.  Avoid death loop.
+--dofile("servernode.lua") --  starts the server script.  comment this out while depugging.  Start the server manually untill you are sure it is working.  Avoid death loop.
+dofile("servernode.lc")
